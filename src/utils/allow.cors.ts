a0098385a56a056112e68aadcde454a85a982b15
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2025-12-25 11:34:04 -0800

    Update allow.cors.ts

--- src/utils/allow.cors.ts
+++ src/utils/allow.cors.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 export const allowingCors = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "https://cap-dashboard.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins for development
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
