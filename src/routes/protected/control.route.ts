2006aba3f93b17ffc1834702a3a6e9ff0d3e7d90
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2026-01-20 11:15:13 -0800

    Update control.route.ts

--- src/routes/protected/control.route.ts
+++ src/routes/protected/control.route.ts
@@ -6,7 +6,7 @@ import { createControl_validation, frameworkId_validation, updateControl_validat
 const router = Router();
 
 router.post('/create', createControl_validation, create);
-router.patch('/update', updateControl_validation, update);
+router.patch('/update/:id', updateControl_validation, update);
 router.get('/list/:frameworkId', frameworkId_validation, findActiveByFramework);
 router.get('/details/:controlId', findByControlIdWithAssessments);
 
